515b22fabf75b44a3f837e89ffe238aaaaf66684
NestorRebo <rebolledonestor@gmail.com>
2023-09-04 12:22:19 -0300

    se agregaron los filtros por check y por texto. Además, de retirar archivos obsoletos

--- tarea 8/script.js
+++ tarea 8/script.js
@@ -13,12 +13,12 @@ function generarTarjetas(dataEventos, ubic) {
 
     cardsHTML +=
       `<div class="card" style="width: 16rem;">
-      <img src="${evento.image}" class="card-img-top" alt="${evento.name}">
+      <img src="${evento.image}" class="card-img-top" style="height: 10rem;" alt="${evento.name}">
       <div class="card-body">
         <h5 class="card-title">${evento.name}</h5>
-        <p class="card-text">${evento.description}</p>
-        <div class="d-flex justify-content-between  align-items-center">
-          <h6>${evento.price}</h6>
+        <p class="card-text" style="height: 5rem;">${evento.description}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <h6>$  ${evento.price}</h6>
           <a href="./assets/pages/details.html" class="btn btn-primary">Details</a>
         </div>
       </div>
@@ -28,20 +28,21 @@ function generarTarjetas(dataEventos, ubic) {
 }
 /* -----------------------------filtrado y envio de las cards a las paginas---------------------------------------- */
 
-/* // generacion de nuevos  arrys con la condicion que queremos
+// generacion de nuevos  arrys con la condicion que queremos
+
 let dataEventosNext = dataEventos.filter(dataEventos => dataEventos.date > fechaActual)
 let dataEventosPast = dataEventos.filter(dataEventos => dataEventos.date < fechaActual)
 
-if(document.title === "Upcoming Events"){
+if (document.title === "Upcoming Events") {
   generarTarjetas(dataEventosNext, cardsNext)
 
-}else if(document.title === "Past Events"){
+} else if (document.title === "Past Events") {
   generarTarjetas(dataEventosPast, cardsPast)
-}else if(document.title === "Amazing Events"){
+} else if (document.title === "Amazing Events") {
   generarTarjetas(dataEventos, cards)
 }
 
- */
+
 
 function impresionDeCards(elementosFiltrados) {
 
@@ -61,27 +62,10 @@ function impresionDeCards(elementosFiltrados) {
     case "Amazing Events":
       generarTarjetas(elementosFiltrados, cards);
       break;
-    default:
-      generarTarjetas(dataEventosNext, cardsNext);
-      generarTarjetas(dataEventosPast, cardsPast);
-      generarTarjetas(dataEventos, cards);
-      break;
 
   }
 
-  /*   if (document.title === "Upcoming Events") {
-      generarTarjetas(dataEventosNext, cardsNext)
-  
-    } else if (document.title === "Past Events") {
-      generarTarjetas(dataEventosPast, cardsPast)
-    } else if (document.title === "Amazing Events") {
-      generarTarjetas(elementosFiltrados, cards)
-    } */
-
 }
-
-// impresionDeCards(dataEventos)
-
 /* --------------------------------------------------------------------- */
 
 
@@ -105,24 +89,27 @@ formulario.addEventListener("submit", (e) => {
 
 /* detectamos si escribieron en la busqueda */
 inputTexto.addEventListener("input", () => {
-  // console.log("escribiendo")
+/*   // console.log("escribiendo")
   let arregloFiltrado = filtrarPorTexto(dataEventos, inputTexto.value)
   // console.log(arregloFiltrado)
-  impresionDeCards(arregloFiltrado)
+  impresionDeCards(arregloFiltrado) */
+  filtroCruzado()
 })
 
-/* ---------------------------------fitro------------------------------------ */
+/* --------------------------------- funcion fitro------------------------------------ */
 
 function filtrarPorTexto(arrayDeObjetos, texto) {
 
-  // Propiedad que deseas eliminar
-  /* let propiedadEliminar1 = 'image'
+  /* no se termino de usar porque necesito estas propiedades para imprimir las cards  
+  Propiedad que deseas eliminar
+  let propiedadEliminar1 = 'image'
   let propiedadEliminar2 = '__v'
- */
-  /* saco las propiedades */
-  /*  let array1 = arrayDeObjetos.map(({ [propiedadEliminar1]: _, ...resto }) => resto);
-   let arrayFiltrado = array1.map(({ [propiedadEliminar2]: _, ...resto }) => resto); */
-  // console.log(arrayFiltrado);
+
+  // saco las propiedade
+  let array1 = arrayDebjetos.map(({ [propiedadEliminar1]: _, ...resto }) => resto);
+  let arrayFiltrado = array1.map(({ [propiedadEliminar2]: _, ...resto }) => resto)
+  console.log(arrayFiltrado); */
+
 
   /* filtro los valores de las propiedades. */
   let arrayProp = arrayDeObjetos.map((e) => (Object.values(e)))
@@ -139,16 +126,20 @@ function filtrarPorTexto(arrayDeObjetos, texto) {
 
 
 
-/* ---------------------------------checkbox------------------------------------ */
+/* ---------------------------------checkboxes------------------------------------ */
 
 /* creo la funcion de para crear checkboxes y uqe lo inserte en los HTML */
 function crearCheckboxes(dataEventos, ubicacion) {
   let checksHTML = "";
-
+  let keysUnicas = [];
   /* vamos buscar los nombres de los checkbox del amazing event.js */
-  let titulosCheckbox = Object.keys(dataEventos[0])
-  let titulosCheckbox1 = titulosCheckbox.filter(e => !e.includes('_') && !e.includes('date') && !e.includes('image'))
+  dataEventos.forEach(e => Object.keys(e).forEach(key => {
+    if (!keysUnicas.includes(key)) {
+      keysUnicas.push(key)
+    }
+  }))
 
+  let titulosCheckbox1 = keysUnicas.filter(e => !e.includes('_') && !e.includes('date') && !e.includes('image'))
 
   for (e of titulosCheckbox1) {
     checksHTML +=
@@ -162,22 +153,53 @@ function crearCheckboxes(dataEventos, ubicacion) {
   ubicacion.innerHTML = checksHTML
 }
 
+
 crearCheckboxes(dataEventos, checkboxes)
 
-/* detectamos si hubo un cambio en los checkboxer */
-checkboxes.addEventListener("change", () => {
-  console.log("cambio un checkbox")
+function filtroCruzado(){
+  let filtradoPorTexto = filtrarPorTexto(dataEventos, inputTexto.value)
+  let filtrarPorTextoYPorCheckers = filtrarPorCheck(filtradoPorTexto)
+  // if(filtrarPorTextoYPorCheckers.length === 0){
+  //   let advertencia= [{
+  //     name: "no se ha encontrado resultado",
+  //     image: "https://i.postimg.cc/PrMJ0ZMc/Metallica-in-concert.jpg",
+  //   }]
+  //   impresionDeCards(advertencia)
+  // }else{
+    impresionDeCards(filtrarPorTextoYPorCheckers)
+  }
 
 
-})
+/* detectamos si hubo un cambio en los checkboxes */
+// checkboxes.addEventListener("change", () => {
+// //  console.log("cambio un checkbox")
+//   //  let arregloFiltradoPorCheck = filtrarPorCheck(dataEventos)
+// //   impresionDeCards(arregloFiltradoPorCheck) 
+//   filtroCruzado()
+// })
+checkboxes.addEventListener("change", () => filtroCruzado())
 
 
+function filtrarPorCheck(arrayDeObjetos) {
+  let checkboxes = document.querySelectorAll("#checkboxes .form-check-input")
+  // console.log(checkboxes)
+  let arrayCheckboxes = Array.from(checkboxes)
+  // console.log(arrayCheckboxes)
+  let checked = arrayCheckboxes.filter(e => e.checked)
+  // console.log(checked)
+  let stringCheck = checked.map(check => check.id)
+  console.log(stringCheck)
 
-function filtrarPorCheck(arregloDeElementos){
-  let checkboxes = document.querySelectorAll("input [type='checkbox']")
-  let arrayCheckboxes = Array.fomr(checkboxes)
-  let checkAzules =
-  console.log([arrayCheckboxes])
+let elementosFiltrados = arrayDeObjetos.filter(obj => Object.keys(obj).some(key => stringCheck.includes(key)))
+console.log(elementosFiltrados)
+
+if(stringCheck.length === 0){
+  return arrayDeObjetos
+}else{
+  return elementosFiltrados
+}
+
+
 
 }
 
